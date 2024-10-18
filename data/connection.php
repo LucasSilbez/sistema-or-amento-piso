cef1a92ab475448e07d82561c2ba6056b381f876
lucassilbez <lucasdsilvab@gmail.comgit config --global user.email lucasdsilvab@gmail.comgit config --global user.email lucasdsilvab@gmail.com>
2024-10-18 18:44:45 -0300

    Feat: inclui dados servidor mysql

--- data/connection.php
+++ data/connection.php
@@ -1,18 +1,18 @@
 <?php
 
+
+$host = 'sql304.infinityfree.com';
+$db = 'if0_37542315_orcamentopiso';
+$user = 'if0_37542315';
+$pass = 'orcamentopiso';
+
+
 /*
-$host = 'sql100.infinityfree.com';
-$db = 'if0_37372284_sos';
-$user = 'if0_37372284';
-$pass = 'sosorcamento123';
-*/
-
-
 $host = 'localhost';
 $db = 'sos';
 $user = 'root';
 $pass = '';
-
+*/
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
